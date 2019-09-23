bf54e15c9474474666644b710e87bf8720704933
Vasu Agrawal <agrawal.vasu2907@gmaill.com>
2019-09-24 03:22:42 +0530

    Search friend On Progress

--- app/src/main/java/com/example/socialcode/SearchFriends.java
+++ app/src/main/java/com/example/socialcode/SearchFriends.java
@@ -3,10 +3,19 @@ package com.example.socialcode;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Window;
+import android.widget.EditText;
+
+import java.util.ArrayList;
 
 public class SearchFriends extends AppCompatActivity {
-
+    RecyclerView recyclerView;
+    private ArrayList<String> arrayList;
+    EditText searchbar;
+    String res = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +28,12 @@ public class SearchFriends extends AppCompatActivity {
         else {
             window.setStatusBarColor(getResources().getColor(R.color.colorPrimary));
         }
+        recyclerView = (RecyclerView) findViewById(R.id.searchfriends_recycler);
+        searchbar =(EditText) findViewById(R.id.searchfriends_search);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(),LinearLayoutManager.VERTICAL));
+        arrayList = new ArrayList<>();
+        
     }
 }
